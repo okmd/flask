1cdc105fade60dc0262e7381c0365468177710d7
okmd <md.softdeveloper@gmail.com>
2019-07-18 16:12:24 +0530

    Completed Assignment

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from Database import Database
 import sqlite3
 import datetime
@@ -10,24 +10,36 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	db = Database()
-	if request=='POST':
+	if request.method=='POST':
 		emp_id = request.form['emp_id']
 		emp_name = request.form['emp_name']
 		emp_position = request.form['emp_position']
-		emp_doj = "08-07-2019"
+		emp_doj = request.form['emp_doj']
 		db.insert(emp_id, emp_name,emp_position, emp_doj)
+		return redirect('/')
 	else:
 		emps = db.select_all()
 		return render_template('index.html',employees=emps)
+
+@app.route('/update/<int:emp_id>', methods=['GET', 'POST'])
+def update(emp_id):
+	db = Database()
+	if request.method=='POST':
+		emp_name = request.form['emp_name']
+		emp_position = request.form['emp_position']
+		emp_doj = request.form['emp_doj']
+		db.update(emp_id, emp_name,emp_position, emp_doj)
+		return redirect('/')
+	else:
+		emps = db.select_one(emp_id)
+		return render_template('update.html',emp=emps)
 
 @app.route('/delete/<int:emp_id>', methods=['GET', 'POST'])
-def delete():
+def delete(emp_id):
 	db = Database()
-	if request=='POST':
-		pass
-	else:
-		emps = db.select_all()
-		return render_template('index.html',employees=emps)
+	db.delete(emp_id)
+	return redirect('/')
+
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
--- Database.py
+++ Database.py
@@ -21,15 +21,40 @@ class Database():
 			self.cursor.execute(self.query)
 			self.cursor.commit()
 		except:
-			print("sorry")
+			print("Unable to create table!")
+			return False
 
 	def insert(self, emp_id, name, position, emp_doj):
-		self.query = '''insert into employees values (?,?,?,?, ?)'''
+		# self.query = "insert into employees values ('{}','{}','{}','{}')".format(emp_id, name, position, emp_doj)
+		self.query = 'insert into employees values (?,?,?,?)'
 		try:
 			self.cursor.execute(self.query,(emp_id, name, position, emp_doj))
-			self.cursor.commit()
+			self.connection.commit()
+			return True
 		except:
 			print("Unable to insert!!")
+			return False
+
+	def update(self, emp_id, name, position, emp_doj):
+		self.query = "update employees set emp_name='{}', emp_position='{}', emp_doj='{}'  where emp_id = '{}' ".format(name, position, emp_doj, emp_id)
+		try:
+			self.cursor.execute(self.query)
+			self.connection.commit()
+			return True
+		except:
+			print("Unable to update!!")
+			return False
+	
+	def delete(self, emp_id):
+			# self.query = "insert into employees values ('{}','{}','{}','{}')".format(emp_id, name, position, emp_doj)
+			self.query = "delete from employees where emp_id = '{}'".format(emp_id)
+			try:
+				self.cursor.execute(self.query)
+				self.connection.commit()
+				return True
+			except:
+				print("Unable to delete!!")
+				return False
 
 	def select_all(self):
 		self.query = "select * from employees"
@@ -38,6 +63,15 @@ class Database():
 			return self.cursor.fetchall()
 		except:
 			print("Unable to select!!")
+			return False
 
-	def __del__(self):
-		self.connection.close()
+
+	def select_one(self, emp_id):
+		self.query = "select * from employees where emp_id = '{}'".format(emp_id)
+		try:
+			self.cursor.execute(self.query)
+			return self.cursor.fetchone()
+		except:
+			print("Unable to select!!")
+			return False
+
